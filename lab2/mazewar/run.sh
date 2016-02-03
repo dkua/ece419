ce4ffe6da1bc524c1e6d54a955a57b67e0db426e
David Kua <david.kua@mail.utoronto.ca>
2016-02-02 23:34:25 -0500

    Updated Makefile and shell scripts for lab comp.

--- lab2/mazewar/run.sh
+++ lab2/mazewar/run.sh
@@ -5,5 +5,9 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+if [ -z "$JAVA_HOME" ]; then
+	JAVA_HOME=/usr
+fi
+
 ${JAVA_HOME}/bin/java Mazewar $1 $2 
 
--- lab2/mazewar/server.sh
+++ lab2/mazewar/server.sh
@@ -5,5 +5,9 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if [ -z "$JAVA_HOME" ]; then
+	JAVA_HOME=/usr
+fi
+
 ${JAVA_HOME}/bin/java Server $1 
 
